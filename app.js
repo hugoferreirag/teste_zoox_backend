3f05b22dfd8d075686e8c64f9d2edd5f45588d98
Hugo Ferreira Mazzatech <hugo.ferreira.mazzatech@infracommerce.com.br>
2020-09-01 12:09:50 -0300

    configurando dot env

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const app = require('express')();
@@ -8,9 +10,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routers);
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2000;
 
 app.listen( port, () => {
-    console.log(' server on')
+    console.log(`server on ${port}`)
 })
--- config/mongodb.js
+++ config/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/zoox', { useNewUrlParser: true,  useUnifiedTopology: true })
+mongoose.connect(process.env.DB, { useNewUrlParser: true,  useUnifiedTopology: true })
     .catch(e => {
         const msg = 'Não foi possível conectar com banco de dados!';
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
